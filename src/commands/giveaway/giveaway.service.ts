98607657679d2e946c8fd466f796871aeacf082a
bielb1b2 <gabrielvt.souza@hotmail.com>
2025-02-13 00:23:53 -0300

    feat: giveaway created

--- src/commands/giveaway/giveaway.service.ts
+++ src/commands/giveaway/giveaway.service.ts
@@ -1,25 +1,50 @@
 import { DateTime } from 'luxon';
 import { IRegisteredDraws, registeredDraws } from './registered-draws'
 import { client } from '../../clientconfig';
+import { TextChannel } from 'discord.js';
 
 export async function cronGiveAway() {
     const gamesOfTheDay: IRegisteredDraws[] = [];
 
     const now = DateTime.now().set({ minute: 0, second: 0, millisecond: 0 });
-    console.log("now", now);
-    console.log("Registered Draws", registeredDraws);
 
     registeredDraws.forEach(item => {
         const executionDate = DateTime.fromISO(item.giveawayInfo.executionDate).set({ minute: 0, second: 0, millisecond: 0 });
-
-
 
         if(executionDate.equals(now)) {
             gamesOfTheDay.push(item);
         }
     })
 
+    if(gamesOfTheDay.length === 0) {
+        return;
+    }
+
     gamesOfTheDay.forEach(item => {
-        
-    })
+        const winners: string[] = [];
+        let participants = item.giveawayInfo.participants;
+        if (participants.length > 0) {
+            for (let index = 0; index < Number(item.giveawayInfo.winners); index++) {
+                const winner = getRandomElement(participants);
+                winners.push(winner);
+                participants = participants.filter(participant => participant !== winner);
+            }
+            const channel = client.channels.cache.get(item.guildInfo.channelId) as TextChannel;
+            if (channel) {
+                channel.send(`Congratulations ${winners.map(winner => `<@${winner}>`).join(', ')}! You have won the giveaway for "${item.giveawayInfo.title}"!`);
+            }
+        } else {
+            const channel = client.channels.cache.get(item.guildInfo.channelId) as TextChannel;
+            if (channel) {
+                channel.send(`No participants for giveaway: "${item.giveawayInfo.title}"!`);
+            }
+        }
+    });
+
+    registeredDraws.length = 0;
+}
+
+function getRandomElement<T>(array: T[]): T {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
 }
