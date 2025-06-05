2c2bc1206edfa96a09086de82f5fcc9c540207ae
bielb1b2 <gabrielvt.souza@hotmail.com>
2025-06-05 00:53:21 -0300

    chore: remove unused imports

--- src/commands/giveaway/giveawayplay.client.ts
+++ src/commands/giveaway/giveawayplay.client.ts
@@ -1,6 +1,5 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, MessageFlags } from "discord.js";
+import { Events } from "discord.js";
 import { client } from "../../clientconfig";
-import { registeredDraws } from "./registered-draws";
 import { cronGiveAway } from "./giveaway.service";
 
 client.on(Events.InteractionCreate, async interaction => {
