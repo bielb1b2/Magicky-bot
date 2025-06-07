f4aa860fa42fee89d5bbca58682f73b8559b6cec
bielb1b2 <gabrielvt.souza@hotmail.com>
2025-06-07 15:26:53 -0300

    feat: add list giveaway commands

---
+++ src/commands/giveaway/giveaway-list.command.ts
@@ -0,0 +1,35 @@
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { ISlashCommand } from "../ISlashCommand";
+import { registeredDraws } from "./registered-draws";
+
+const giveawayListCommand: ISlashCommand = {
+    data: new SlashCommandBuilder()
+        .setName("giveaway-list")
+        .setDescription("List all giveaways"),
+    async execute(interaction) {
+        const allDraws = registeredDraws.filter(item => item.guildInfo.guildId === interaction.guildId);
+
+        if(allDraws.length <= 0) {
+            await interaction.reply("No giveaway created yet")
+        }
+
+        const embedMessage = new EmbedBuilder()
+            .setTitle(`All giveaways: ${interaction.guild?.name}`)
+            .setDescription("Show all giveaways that was not played in server yet")
+
+
+        allDraws.forEach((item) => {
+            embedMessage.addFields(
+                { name: item.giveawayInfo.title, value: item.giveawayInfo.description }
+            )
+        })
+
+        await interaction.reply({
+            embeds: [embedMessage]
+        })
+
+        return
+    }
+}
+
+export { giveawayListCommand }
--- src/commands/loadCommands.ts
+++ src/commands/loadCommands.ts
@@ -3,12 +3,14 @@ import { ISlashCommand } from "./ISlashCommand";
 // Import all commands here
 import { pingCommand } from "./utility/ping.command";
 import { giveawayCommand } from "./giveaway/giveaway.command";
+import { giveawayListCommand } from "./giveaway/giveaway-list.command";
 import { reloadCommand } from "./utility/reload.command";
 
 const slashCommands: ISlashCommand[] = [];
 
 slashCommands.push(
     pingCommand,
+    giveawayListCommand,
     giveawayCommand,
     reloadCommand
 )
