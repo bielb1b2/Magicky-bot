2cf1be507988cb8c8424feaa8df639af1007d4e5
bielb1b2 <gabrielvt.souza@hotmail.com>
2025-06-11 00:16:58 -0300

    fix: just me can use the reload command

--- src/commands/utility/reload.command.ts
+++ src/commands/utility/reload.command.ts
@@ -1,6 +1,6 @@
 import { Routes, SlashCommandBuilder } from "discord.js";
-import { ISlashCommand } from "../ISlashCommand";
 import { rest } from "../../clientconfig";
+import { ISlashCommand } from "../ISlashCommand";
 import { slashCommands } from "../loadCommands";
 
 const reloadCommand: ISlashCommand = {
@@ -12,6 +12,12 @@ const reloadCommand: ISlashCommand = {
             Routes.applicationCommands("1338318534660329552"),
             { body: slashCommands.map(command => command.data) }
         );
+        
+        if(interaction.user.id !== "211612940191793153") {
+            await interaction.reply({ content: "You do not have permission to use this command.", ephemeral: true });  
+            await interaction.followUp({ content: "Only the bot owner can use this command." });
+            return;
+        }
 
         await interaction.reply("Commands reloaded successfully! The bot will restart now.");
     }
